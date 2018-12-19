d2875710f1324a5d892e3c69b4299321856adfa2
Tilly Oswell-Wheeler <tilly.oswellwheeler@hotmail.com>
2018-12-19 09:28:12 +0000

    Adding the course files to the repo

---
+++ CreateApp/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+              short shortVal = 7;
+              long longVal = 5;
+
+              short resul1 = (short) longVal;
+
+              System.out.println("Success");
+    }
+}
